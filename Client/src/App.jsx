669e6d847cd904a1f41d2a7d424faef2f1e4927e
Himanshu_Singh <06hsr90@gmail.com>
2023-08-09 17:54:58 +0530

    added basic layout (Client)

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,22 @@
 
+import React from "react"
+import {BrowserRouter , Routes , Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Contact from "./pages/Contact"
+import About from "./pages/About"
+import Layout from "./components/Layout"
 function App() {
 
     return (
-        <>
-            <h1>App</h1>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route index element={<Home/>}/>
+                    <Route path="about" element={<About/>}/>
+                    <Route path="contact" element={<Contact/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
