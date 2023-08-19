8ca087b3b51a26f1d3e474a0306deb9dfbf2dea6
Himanshu_Singh <06hsr90@gmail.com>
2023-08-19 18:47:11 +0530

    made header sticky (Client)

--- Client/src/components/Header/Header.jsx
+++ Client/src/components/Header/Header.jsx
@@ -4,13 +4,33 @@ import LeftHeader from './LeftHeader'
 import MiddleHeader from './MiddleHeader'
 import RightHeader from './RightHeader'
 import Part2 from './Part2'
+import { useEffect, useRef, useState } from 'react'
 
 function Header() {
+
+    let [isSticky , setIsSticky] = useState(false)
+    let part1Ref = useRef(null)
+
+    useEffect(function(){
+
+        function handleScroll(){
+            let part1Height = part1Ref.current.offsetHeight
+            if (window.scrollY > part1Height)
+                setIsSticky(true)
+            else
+                setIsSticky(false)
+        }
+
+        window.addEventListener('scroll' , handleScroll)
+        return function(){
+            window.removeEventListener('scroll' , handleScroll)
+        }
+    } , [])
 
     return (
         <>
             <div className={styles.header}>
-                <div className={styles.part1}>
+                <div ref={part1Ref} className={`${styles.part1} ${isSticky ? `${styles.sticky}` : ''}`}>
                     <LeftHeader/>
                     <MiddleHeader/>
                     <RightHeader/>
