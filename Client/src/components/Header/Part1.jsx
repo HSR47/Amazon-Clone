a6434d95aeec4436d1f6b4e741e6fe5c80f4146a
Himanshu_Singh <06hsr90@gmail.com>
2023-10-04 19:39:52 +0530

    minor changes (Client)

--- Client/src/components/Header/Part1.jsx
+++ Client/src/components/Header/Part1.jsx
@@ -3,26 +3,30 @@ import LeftHeader from './LeftHeader'
 import MiddleHeader from './MiddleHeader'
 import RightHeader from './RightHeader'
 import styles from './Header.module.css'
+import { useLocation } from 'react-router-dom';
 
 function Part1() {
     let [isSticky , setIsSticky] = useState(false)
     let part1Ref = useRef(null)
+    let location = useLocation()
 
     useEffect(function(){
-
-        function handleScroll(){
-            let part1Height = part1Ref.current.offsetHeight
-            if (window.scrollY > part1Height)
-                setIsSticky(true)
-            else
-                setIsSticky(false)
+        if (location.pathname == '/')
+        {
+            function handleScroll(){
+                let part1Height = part1Ref.current.offsetHeight
+                if (window.scrollY > part1Height)
+                    setIsSticky(true)
+                else
+                    setIsSticky(false)
+            }
+    
+            window.addEventListener('scroll' , handleScroll)
+            return function(){
+                window.removeEventListener('scroll' , handleScroll)
+            }
         }
-
-        window.addEventListener('scroll' , handleScroll)
-        return function(){
-            window.removeEventListener('scroll' , handleScroll)
-        }
-    } , [])
+    } , [location])
 
     return (
         <div ref={part1Ref} className={`${styles.part1} ${isSticky ? `${styles.sticky}` : ''}`}>
--- Client/src/pages/Product/Image.jsx
+++ Client/src/pages/Product/Image.jsx
@@ -1,9 +1,30 @@
 import styles from './ProductPage.module.css'
+import { useState } from 'react'
 
-function Image(){
+
+function Image({ product }) {
+    let [activeImg, setActiveImg] = useState(0)
+
+    function handleImgClick(e){
+        setActiveImg(e.target.dataset.index)
+    }
     return (
         <div className={styles.image}>
-            <h1>Image</h1>
+            <div className={styles.container}>
+                <ul>
+                    {product.images.map(function (i, ind) {
+                        return (
+                            <li key={ind}>
+                                <img data-index={ind} src={i} onMouseEnter={handleImgClick}/>
+                            </li>
+                        )
+                    })}
+                </ul>
+
+                <div className={styles.active}>
+                    <img src={product.images[activeImg]} />
+                </div>
+            </div>
         </div>
     )
 }
